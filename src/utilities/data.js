d1146da9de99d40b59ff546a85081d841baa42f1
Peter Krevenets <peter.k.nets@gmail.com>
2017-06-28 09:58:18 +0300

    Rearranged projects from new to old

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -3,67 +3,29 @@ export const skills = ['JavaScript', 'ES6', 'React', 'Redux',
 
 export const projects = [
   {
-    id: 1,
-    title: 'Tribute Page',
-    description: 'freeCodeCamp Basic Front-End Development Project',
-    goal: `Build a Tribute Page with an image and text. It should also contain
-      a link that takes user to an external website with further information on
-      the topic.`,
-    technologies: ['JavaScript', 'CSS', 'Bootstrap'],
-    href: 'https://gurugumawaru.github.io/FCC_StephenKingTribute/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1492425629/FCC_Stephen-King_resized_mgnfei.jpg',
-    screenshotLabel: 'Tribute Page'
+    id: 8,
+    title: 'TicTacToe Game',
+    description: 'Free Code Camp Advanced Front-End Development Project',
+    goal: `Build a Tic Tac Toe Game. A user can play a game of Tic Tac
+      Toe with the computer. User game will reset as soon as it's over so
+      he/she can play again. User can choose whether to play as X or O.`,
+    technologies: ['ES6', 'React', 'Redux'],
+    href: 'https://gurugumawaru.github.io/FCC_ticTacToe/',
+    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1498579433/FCC_TicTacToe_resized_kux8jv.png',
+    screenshotLabel: 'TicTacToe Game'
   },
   {
-    id: 2,
-    title: 'Random Quote Machine',
-    description: 'Free Code Camp Basic Front-End Development Project',
-    goal: `Build a Random Quote Machine. A user can click a button
-      to show a new random quote. There is also a button that allows to tweet
-      out a quote.`,
-    technologies: ['JavaScript', 'jQuery', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_random_quotes/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493112050/FCC_Random-Quote_resized_2_gkowtc.png',
-    screenshotLabel: 'Random Quote Machine'
-  },
-  {
-    id: 3,
-    title: 'Local Weather App',
-    description: 'Free Code Camp Intermediate Front-End Development Project',
-    goal: `Build a Local Weather app. It should show weather at current location.
-      There should be a different icon or background image depending on the
-      weather. There must be a button to toggle between Fahrenheit and Celcius.`,
-    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_weather/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493111811/FCC_Weather-App_resized_2_x87yny.png',
-    screenshotLabel: 'Local Weather App'
-  },
-  {
-    id: 4,
-    title: 'Wikipedia Viewer',
-    description: 'Free Code Camp Intermediate Front-End Development Project',
-    goal: `Build a Wikipedia Viewer. It should allow to search Wikipedia
-      entries in a search box and see the resulting Wikipedia entries.
-      There should be a button that can be clicked to get a random
-      Wikipedia article.`,
-    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_wiki_viewer/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493112694/FCC_Wiki_Viewer_resized_r0fcmb.png',
-    screenshotLabel: 'Wikipedia Viewer'
-  },
-  {
-    id: 5,
-    title: 'Twitchtv Viewer',
-    description: 'Free Code Camp Intermediate Front-End Development Project',
-    goal: `Use a Twitchtv JSON API to build an app. A user should see whether
-      Free Code Camp is currently streaming. If a Twitch user is streaming,
-      there should be additional details about what they are streaming.
-      A placeholder notifiction should be displayed If a streamer has closed
-      their Twitch account (or the account never existed).`,
-    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_twitch_tv/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493116898/FCC_Twitchtv_resized_o8ghsw.jpg',
-    screenshotLabel: 'Twitchtv Viewer'
+    id: 7,
+    title: 'Pomodoro App',
+    description: 'Free Code Camp Advanced Front-End Development Project',
+    goal: `Build a Pomodoro Clock. A user can start a 25 minute pomodoro,
+      and the timer will go off once 25 minutes has elapsed.
+      A user can reset the clock for his/her next pomodoro. A user can
+      customize the length of each pomodoro.`,
+    technologies: ['ES6', 'React', 'Redux'],
+    href: 'https://gurugumawaru.github.io/FCC_pomodoro/',
+    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1495016958/FCC_Pomodoro_Clock_resized_v7eext.png',
+    screenshotLabel: 'Pomodoro App'
   },
   {
     id: 6,
@@ -80,28 +42,66 @@ export const projects = [
     screenshotLabel: 'JavaScript Calculator'
   },
   {
-    id: 7,
-    title: 'Pomodoro App',
-    description: 'Free Code Camp Advanced Front-End Development Project',
-    goal: `Build a Pomodoro Clock. A user can start a 25 minute pomodoro,
-      and the timer will go off once 25 minutes has elapsed.
-      A user can reset the clock for his/her next pomodoro. A user can
-      customize the length of each pomodoro.`,
-    technologies: ['ES6', 'React', 'Redux'],
-    href: 'https://gurugumawaru.github.io/FCC_pomodoro/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1495016958/FCC_Pomodoro_Clock_resized_v7eext.png',
-    screenshotLabel: 'Pomodoro App'
+    id: 5,
+    title: 'Twitchtv Viewer',
+    description: 'Free Code Camp Intermediate Front-End Development Project',
+    goal: `Use a Twitchtv JSON API to build an app. A user should see whether
+      Free Code Camp is currently streaming. If a Twitch user is streaming,
+      there should be additional details about what they are streaming.
+      A placeholder notifiction should be displayed If a streamer has closed
+      their Twitch account (or the account never existed).`,
+    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
+    href: 'https://gurugumawaru.github.io/FCC_twitch_tv/',
+    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493116898/FCC_Twitchtv_resized_o8ghsw.jpg',
+    screenshotLabel: 'Twitchtv Viewer'
   },
   {
-    id: 8,
-    title: 'TicTacToe Game',
-    description: 'Free Code Camp Advanced Front-End Development Project',
-    goal: `Build a Tic Tac Toe Game. A user can play a game of Tic Tac
-      Toe with the computer. User game will reset as soon as it's over so
-      he/she can play again. User can choose whether to play as X or O.`,
-    technologies: ['ES6', 'React', 'Redux'],
-    href: 'https://gurugumawaru.github.io/FCC_ticTacToe/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1498579433/FCC_TicTacToe_resized_kux8jv.png',
-    screenshotLabel: 'TicTacToe Game'
+    id: 4,
+    title: 'Wikipedia Viewer',
+    description: 'Free Code Camp Intermediate Front-End Development Project',
+    goal: `Build a Wikipedia Viewer. It should allow to search Wikipedia
+      entries in a search box and see the resulting Wikipedia entries.
+      There should be a button that can be clicked to get a random
+      Wikipedia article.`,
+    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
+    href: 'https://gurugumawaru.github.io/FCC_wiki_viewer/',
+    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493112694/FCC_Wiki_Viewer_resized_r0fcmb.png',
+    screenshotLabel: 'Wikipedia Viewer'
+  },
+  {
+    id: 3,
+    title: 'Local Weather App',
+    description: 'Free Code Camp Intermediate Front-End Development Project',
+    goal: `Build a Local Weather app. It should show weather at current location.
+      There should be a different icon or background image depending on the
+      weather. There must be a button to toggle between Fahrenheit and Celcius.`,
+    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
+    href: 'https://gurugumawaru.github.io/FCC_weather/',
+    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493111811/FCC_Weather-App_resized_2_x87yny.png',
+    screenshotLabel: 'Local Weather App'
+  },
+  {
+    id: 2,
+    title: 'Random Quote Machine',
+    description: 'Free Code Camp Basic Front-End Development Project',
+    goal: `Build a Random Quote Machine. A user can click a button
+      to show a new random quote. There is also a button that allows to tweet
+      out a quote.`,
+    technologies: ['JavaScript', 'jQuery', 'SCSS'],
+    href: 'https://gurugumawaru.github.io/FCC_random_quotes/',
+    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493112050/FCC_Random-Quote_resized_2_gkowtc.png',
+    screenshotLabel: 'Random Quote Machine'
+  },
+  {
+    id: 1,
+    title: 'Tribute Page',
+    description: 'freeCodeCamp Basic Front-End Development Project',
+    goal: `Build a Tribute Page with an image and text. It should also contain
+      a link that takes user to an external website with further information on
+      the topic.`,
+    technologies: ['JavaScript', 'CSS', 'Bootstrap'],
+    href: 'https://gurugumawaru.github.io/FCC_StephenKingTribute/',
+    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1492425629/FCC_Stephen-King_resized_mgnfei.jpg',
+    screenshotLabel: 'Tribute Page'
   },
 ];
