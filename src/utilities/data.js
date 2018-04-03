d4cadf34b2eb51ac76283d3acd95dc1a9c3a1ceb
Peter Krevenets <peter.k.nets@gmail.com>
2018-04-03 17:46:05 +0300

    Add new projects

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -1,48 +1,94 @@
-export const skills = ['JavaScript', 'ES6', 'React', 'Redux',
-'jQuery', 'HTML5', 'CSS3', 'SCSS', 'Webpack'];
+export const skills = [
+  "JavaScript",
+  "ES6",
+  "React",
+  "React Redux",
+  "Redux Thunk",
+  "MongoDB",
+  "Express",
+  "jQuery",
+  "HTML5",
+  "CSS3",
+  "SCSS",
+  "Webpack"
+];
 
 export const projects = [
   {
+    id: 14,
+    title: "Game of Life",
+    description: "Free Code Camp React Project",
+    goal: `When User first arrives at the game, it will randomly generate a board and start playing. User can start and stop the board. User can set up the board. User can clear the board. When User presses start, the game will play out. Each time the board changes, User can see how many generations have gone by.`,
+    technologies: ["ES6", "React", "React Redux", "Redux Thunk"],
+    href: "https://gurugumawaru.github.io/FCC_game_of_life/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1522765892/FCC_Game_Of_Life_resized_ddofex.png",
+    screenshotLabel: "Game of Life"
+  },
+  {
+    id: 13,
+    title: "Event Calendar",
+    description: "React Project for Interview Test",
+    goal: `Conflicting events should be the same width, and should not overlap. Max event width is 200px. Event background color is #E2ECF5 with #6E9ECF border color. Event font is Open Sans, 14px. Calendar time font is Open Sans with around 200 font weight, 16px large, 12px small. Titles should be 1 line and should not overflow outside the calendar-event box. If the title is too long to fit, ellipsis (“…”) should be used. Events will be between 8am to 5pm (there’s a label for 5pm but events will not exceed 5pm). Calendar will cover only one day. User should be able to add/remove events from his calendar. User should be able to export his calendar in JSON (specified format). User should be able to login and persist his inputs between logins.`,
+    technologies: [
+      "ES6",
+      "React",
+      "React Redux",
+      "Redux Thunk",
+      "Passport.js",
+      "Express",
+      "MongoDB",
+      "Material-UI"
+    ],
+    href: "https://damp-meadow-56363.herokuapp.com/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1520769538/Test__Event-Calendar_wcjvnl.png",
+    screenshotLabel: "Event Calendar"
+  },
+  {
     id: 12,
-    title: 'Recipe Box',
-    description: 'Free Code Camp React Project',
+    title: "Recipe Box",
+    description: "Free Code Camp React Project",
     goal: `User can create recipes that have names and ingredients.
     User can see an index view where the names of all the recipes are visible.
     User can click into any of those recipes to view it.
     User can edit these recipes. User can delete these recipes.
     All new recipes user adds are saved in browser's local storage. If user refreshes the page, these recipes will still be there.`,
-    technologies: ['ES6', 'React', 'Redux', 'Material-UI'],
-    href: 'https://gurugumawaru.github.io/FCC_recipe_box/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1503127487/FCC_RecipeBox_resized_laqlnu.png',
-    screenshotLabel: 'Recipe Box'
+    technologies: ["ES6", "React", "Redux", "Material-UI"],
+    href: "https://gurugumawaru.github.io/FCC_recipe_box/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1503127487/FCC_RecipeBox_resized_laqlnu.png",
+    screenshotLabel: "Recipe Box"
   },
   {
     id: 11,
-    title: 'Camper Leaderboard',
-    description: 'Free Code Camp React Project',
+    title: "Camper Leaderboard",
+    description: "Free Code Camp React Project",
     goal: `User can see a table of the freeCodeCamp campers who've earned the most brownie points in the past 30 days.
     User can see how many brownie points they've earned in the past 30 days, and how many they've earned total.
     User can toggle between sorting the list by how many brownie points they've earned in the past 30 days and by how many brownie points they've earned total.`,
-    technologies: ['ES6', 'React', 'Redux', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_camper_leaderboard/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1501691208/FCC_Camper_Leaderboard_resized_of8hrf.jpg',
-    screenshotLabel: 'Camper Leaderboard'
+    technologies: ["ES6", "React", "Redux", "SCSS"],
+    href: "https://gurugumawaru.github.io/FCC_camper_leaderboard/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1501691208/FCC_Camper_Leaderboard_resized_of8hrf.jpg",
+    screenshotLabel: "Camper Leaderboard"
   },
   {
     id: 10,
-    title: 'Markdown Previewer',
-    description: 'Free Code Camp React Project',
+    title: "Markdown Previewer",
+    description: "Free Code Camp React Project",
     goal: `User can type GitHub-flavored Markdown into a text area.
     User can see a preview of the output of his or her markdown that is updated as he or she types`,
-    technologies: ['ES6', 'React', 'Redux', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_markdown_previewer/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1500576485/FCC_Markdown_Previewer_resized_og4gon.jpg',
-    screenshotLabel: 'Markdown Previewer'
+    technologies: ["ES6", "React", "Redux", "SCSS"],
+    href: "https://gurugumawaru.github.io/FCC_markdown_previewer/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1500576485/FCC_Markdown_Previewer_resized_og4gon.jpg",
+    screenshotLabel: "Markdown Previewer"
   },
   {
     id: 9,
-    title: 'Simon Game',
-    description: 'Free Code Camp Advanced Front-End Development Project',
+    title: "Simon Game",
+    description: "Free Code Camp Advanced Front-End Development Project",
     goal: `User is presented with a random series of button presses.
       Each time user inputs a series of button presses correctly,
       he/she sees the same series of button presses but with an additional step.
@@ -59,111 +105,120 @@ export const projects = [
       random series of button presses. User can win the game by getting
       a series of 20 steps correct. User is notified of his/her victory,
       then the game starts over.`,
-    technologies: ['ES6', 'React', 'Redux'],
-    href: 'https://gurugumawaru.github.io/FCC_Simon_Game/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1499783455/FCC_Simon_Game_resized_gezwj9.jpg',
-    screenshotLabel: 'Simon Game'
+    technologies: ["ES6", "React", "Redux"],
+    href: "https://gurugumawaru.github.io/FCC_Simon_Game/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1499783455/FCC_Simon_Game_resized_gezwj9.jpg",
+    screenshotLabel: "Simon Game"
   },
   {
     id: 8,
-    title: 'TicTacToe Game',
-    description: 'Free Code Camp Advanced Front-End Development Project',
+    title: "TicTacToe Game",
+    description: "Free Code Camp Advanced Front-End Development Project",
     goal: `Build a Tic Tac Toe Game. A user can play a game of Tic Tac
       Toe with the computer. User game will reset as soon as it's over so
       he/she can play again. User can choose whether to play as X or O.`,
-    technologies: ['ES6', 'React', 'Redux'],
-    href: 'https://gurugumawaru.github.io/FCC_ticTacToe/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1498579433/FCC_TicTacToe_resized_kux8jv.png',
-    screenshotLabel: 'TicTacToe Game'
+    technologies: ["ES6", "React", "Redux"],
+    href: "https://gurugumawaru.github.io/FCC_ticTacToe/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1498579433/FCC_TicTacToe_resized_kux8jv.png",
+    screenshotLabel: "TicTacToe Game"
   },
   {
     id: 7,
-    title: 'Pomodoro App',
-    description: 'Free Code Camp Advanced Front-End Development Project',
+    title: "Pomodoro App",
+    description: "Free Code Camp Advanced Front-End Development Project",
     goal: `Build a Pomodoro Clock. A user can start a 25 minute pomodoro,
       and the timer will go off once 25 minutes has elapsed.
       A user can reset the clock for his/her next pomodoro. A user can
       customize the length of each pomodoro.`,
-    technologies: ['ES6', 'React', 'Redux'],
-    href: 'https://gurugumawaru.github.io/FCC_pomodoro/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1495016958/FCC_Pomodoro_Clock_resized_v7eext.png',
-    screenshotLabel: 'Pomodoro App'
+    technologies: ["ES6", "React", "Redux"],
+    href: "https://gurugumawaru.github.io/FCC_pomodoro/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1495016958/FCC_Pomodoro_Clock_resized_v7eext.png",
+    screenshotLabel: "Pomodoro App"
   },
   {
     id: 6,
-    title: 'JavaScript Calculator',
-    description: 'Free Code Camp Advanced Front-End Development Project',
+    title: "JavaScript Calculator",
+    description: "Free Code Camp Advanced Front-End Development Project",
     goal: `Build a JavaScript Calculator. A user can add, subtract,
       multiply and divide two numbers. A user can clear the input field
       with a clear button. A user can keep chaining mathematical operations
       together until he/she hits the equal button, and the calculator
       will tell him/her the correct output.`,
-    technologies: ['ES6', 'React', 'Redux'],
-    href: 'https://gurugumawaru.github.io/FCC_calculator/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493117490/FCC_Calculator_resized_olupov.png',
-    screenshotLabel: 'JavaScript Calculator'
+    technologies: ["ES6", "React", "Redux"],
+    href: "https://gurugumawaru.github.io/FCC_calculator/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1493117490/FCC_Calculator_resized_olupov.png",
+    screenshotLabel: "JavaScript Calculator"
   },
   {
     id: 5,
-    title: 'Twitchtv Viewer',
-    description: 'Free Code Camp Intermediate Front-End Development Project',
+    title: "Twitchtv Viewer",
+    description: "Free Code Camp Intermediate Front-End Development Project",
     goal: `Use a Twitchtv JSON API to build an app. A user should see whether
       Free Code Camp is currently streaming. If a Twitch user is streaming,
       there should be additional details about what they are streaming.
       A placeholder notifiction should be displayed If a streamer has closed
       their Twitch account (or the account never existed).`,
-    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_twitch_tv/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493116898/FCC_Twitchtv_resized_o8ghsw.jpg',
-    screenshotLabel: 'Twitchtv Viewer'
+    technologies: ["ES6", "jQuery", "React", "SCSS"],
+    href: "https://gurugumawaru.github.io/FCC_twitch_tv/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1493116898/FCC_Twitchtv_resized_o8ghsw.jpg",
+    screenshotLabel: "Twitchtv Viewer"
   },
   {
     id: 4,
-    title: 'Wikipedia Viewer',
-    description: 'Free Code Camp Intermediate Front-End Development Project',
+    title: "Wikipedia Viewer",
+    description: "Free Code Camp Intermediate Front-End Development Project",
     goal: `Build a Wikipedia Viewer. It should allow to search Wikipedia
       entries in a search box and see the resulting Wikipedia entries.
       There should be a button that can be clicked to get a random
       Wikipedia article.`,
-    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_wiki_viewer/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493112694/FCC_Wiki_Viewer_resized_r0fcmb.png',
-    screenshotLabel: 'Wikipedia Viewer'
+    technologies: ["ES6", "jQuery", "React", "SCSS"],
+    href: "https://gurugumawaru.github.io/FCC_wiki_viewer/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1493112694/FCC_Wiki_Viewer_resized_r0fcmb.png",
+    screenshotLabel: "Wikipedia Viewer"
   },
   {
     id: 3,
-    title: 'Local Weather App',
-    description: 'Free Code Camp Intermediate Front-End Development Project',
+    title: "Local Weather App",
+    description: "Free Code Camp Intermediate Front-End Development Project",
     goal: `Build a Local Weather app. It should show weather at current location.
       There should be a different icon or background image depending on the
       weather. There must be a button to toggle between Fahrenheit and Celcius.`,
-    technologies: ['ES6', 'jQuery', 'React', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_weather/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493111811/FCC_Weather-App_resized_2_x87yny.png',
-    screenshotLabel: 'Local Weather App'
+    technologies: ["ES6", "jQuery", "React", "SCSS"],
+    href: "https://gurugumawaru.github.io/FCC_weather/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1493111811/FCC_Weather-App_resized_2_x87yny.png",
+    screenshotLabel: "Local Weather App"
   },
   {
     id: 2,
-    title: 'Random Quote Machine',
-    description: 'Free Code Camp Basic Front-End Development Project',
+    title: "Random Quote Machine",
+    description: "Free Code Camp Basic Front-End Development Project",
     goal: `Build a Random Quote Machine. A user can click a button
       to show a new random quote. There is also a button that allows to tweet
       out a quote.`,
-    technologies: ['JavaScript', 'jQuery', 'SCSS'],
-    href: 'https://gurugumawaru.github.io/FCC_random_quotes/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1493112050/FCC_Random-Quote_resized_2_gkowtc.png',
-    screenshotLabel: 'Random Quote Machine'
+    technologies: ["JavaScript", "jQuery", "SCSS"],
+    href: "https://gurugumawaru.github.io/FCC_random_quotes/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1493112050/FCC_Random-Quote_resized_2_gkowtc.png",
+    screenshotLabel: "Random Quote Machine"
   },
   {
     id: 1,
-    title: 'Tribute Page',
-    description: 'freeCodeCamp Basic Front-End Development Project',
+    title: "Tribute Page",
+    description: "freeCodeCamp Basic Front-End Development Project",
     goal: `Build a Tribute Page with an image and text. It should also contain
       a link that takes user to an external website with further information on
       the topic.`,
-    technologies: ['JavaScript', 'CSS', 'Bootstrap'],
-    href: 'https://gurugumawaru.github.io/FCC_StephenKingTribute/',
-    screenshotLink: 'https://res.cloudinary.com/gurugumawaru/image/upload/v1492425629/FCC_Stephen-King_resized_mgnfei.jpg',
-    screenshotLabel: 'Tribute Page'
-  },
+    technologies: ["JavaScript", "CSS", "Bootstrap"],
+    href: "https://gurugumawaru.github.io/FCC_StephenKingTribute/",
+    screenshotLink:
+      "https://res.cloudinary.com/gurugumawaru/image/upload/v1492425629/FCC_Stephen-King_resized_mgnfei.jpg",
+    screenshotLabel: "Tribute Page"
+  }
 ];
